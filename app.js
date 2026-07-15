6fefd60264b1ef52c838e96999a0f6db219a67d5
Abhishek Kapse <kapseabhishek50@gmail.com>
2026-07-15 13:06:24 +0530

    New user model added and Authentication

--- app.js
+++ app.js
@@ -8,9 +8,16 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+
+
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
+
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 main().then(()=>{
@@ -49,8 +56,23 @@ app.get("/", (req,res)=>{
     res.send("I'am Root ");
 });
 
+
+
+//Session and Cookies
+
 app.use(session(sessionOptions));
 app.use(flash());
+
+
+//Authentication and Authorizetion
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 
 app.use((req, res, next)=>{
     res.locals.success = req.flash("success");
@@ -58,10 +80,23 @@ app.use((req, res, next)=>{
     next();
 });
 
+// app.get("/demouser", async (req, res)=>{
+//    let fakeUser =  new User({
+//     email : "student@gmail.com",
+//     username : "delta-student",
+//    });
 
-app.use("/listings", listings);
+//    let registeredUser = await User.register(fakeUser, "helloworld");
+//    res.send(registeredUser);
+// });
 
-app.use("/listings/:id/reviews", reviews);
+
+
+app.use("/listings", listingRouter);
+
+app.use("/listings/:id/reviews", reviewRouter);
+
+app.use("/", userRouter);
 
 app.all(/.*/ ,(req, res, next)=>{
     next(new ExpressError(404, "Page Not Found"));
